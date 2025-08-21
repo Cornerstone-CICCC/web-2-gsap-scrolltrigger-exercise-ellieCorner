260c02de35beed70ebfcf8c11ca313e921fc3dd9
ellieCorner <ellie25corner@gmail.com>
2025-08-20 18:08:16 -0700

    Web 2 - GSAP Part 2 Exercise

--- app.js
+++ app.js
@@ -1,3 +1,92 @@
-gsap.registerPlugin(ScrollTrigger)
+const initScrollAnimations = () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-// YOUR CODE HERE
+  const THEME_COLORS = {
+    light: "#FDEE4E",
+    white: "#FFFFFF",
+    black: "#000000",
+  };
+
+  gsap.context(() => {
+    gsap.set("section span", { color: THEME_COLORS.black });
+
+    setupLightModeTimeline(THEME_COLORS);
+    setupOrTimeline();
+    setupDarkModeTimeline(THEME_COLORS);
+  });
+};
+
+const setupLightModeTimeline = (colors) => {
+  const lightModeTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".line-one",
+      start: "top top",
+      end: "+=3000",
+      scrub: 1,
+      pin: true,
+    },
+  });
+
+  lightModeTl
+    .from(".line-one span", { y: 100, opacity: 0, stagger: 0.1 })
+    .to("body", { backgroundColor: colors.light, duration: 1 })
+    .from(".sun", { x: 300, opacity: 0, duration: 1 })
+    .to({}, { duration: 0.5 })
+    .to(
+      ".line-one span",
+      { x: 300, opacity: 0, stagger: 0.1, duration: 2 },
+      "exit"
+    )
+    .to(".sun", { x: -300, opacity: 0, duration: 2 }, "exit")
+    .to("body", { backgroundColor: colors.white, duration: 2 }, "exit");
+};
+
+const setupOrTimeline = () => {
+  const orTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".line-two",
+      start: "top top",
+      end: "+=1500",
+      scrub: 1,
+      pin: true,
+    },
+  });
+
+  orTl
+    .from(".line-two span", { scale: 15, opacity: 0, stagger: 0.2 })
+    .to(".line-two span:first-child", {
+      y: -25,
+      repeat: 1,
+      yoyo: true,
+      duration: 0.3,
+      ease: "power1.inOut",
+    })
+    .to(".line-two span:last-child", {
+      y: -25,
+      repeat: 1,
+      yoyo: true,
+      duration: 0.3,
+      ease: "power1.inOut",
+    })
+    .to(".line-two span", { y: -100, opacity: 0, stagger: 0.1 });
+};
+
+const setupDarkModeTimeline = (colors) => {
+  const darkModeTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".line-three",
+      start: "top top",
+      end: "+=2000",
+      scrub: 1,
+      pin: true,
+    },
+  });
+
+  darkModeTl
+    .from(".line-three span", { y: 100, opacity: 0, stagger: 0.2 }, "<0.2")
+    .to(".line-three", { backgroundPositionX: "0%" })
+    .to(".line-three span", { color: colors.white }, "<10%")
+    .from(".moon", { x: 300, opacity: 0, duration: 1 }, "<");
+};
+
+initScrollAnimations();
